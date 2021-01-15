a8017ac28f7152ebfa811b7d79651e025ca61297
Tahmina <tahmina.ringer@gmail.com>
2021-01-14 17:46:13 -0800

    pushing deployment changes

---
+++ __test__/test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import form from './form';
+import App from './App';
+import result from './result'
+
+test('true is true', () => {
+  expect(true).toBe(true);
+})
+
+
